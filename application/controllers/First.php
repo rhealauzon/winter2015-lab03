438028121da422f6fc69ba0eda5d96de5b88ab18
Rhea <rhealauzon@gmail.com>
2015-01-20 18:14:50 -0800

    Setup starter quotes webapp Signed-off-by:Rhea <rhealauzon@gmail.com>

---
+++ application/controllers/First.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class Welcome extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() 
+    {
+         // this is the view we want shown
+        $this->data['pagebody'] = 'justone';
+
+        $this->render();
+    }
+
+}
+
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
